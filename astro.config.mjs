1e93dfdcf7b6bbe1ed86a9041d36796c0d4f3d41
Roberto Bulnes <rdbv87@gmail.com>
2026-09-18 10:56:10 -0600

    ci: configure GitHub Pages automated deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rdbv87.github.io',
+  base: '/tierraindia',
   vite: {
     plugins: [tailwindcss()]
   }
